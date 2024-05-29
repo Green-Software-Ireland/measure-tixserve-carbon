258d4f39658f575fd7a90ba209d96285b0d3f6e9
John Behan <behanj@gmail.com>
2024-05-29 15:41:08 +0100

    initial commit - measurement for May 24

---
+++ README.md
@@ -0,0 +1,11 @@
+teads-aws
+sum:
+sci:
+
+Calculate SCI as an aggregate
+~~Get the operational carbon~~
+~~Use the Watt Time plugin~~
+
+Can we aggregate the embodied carbon
+use teads
+measure ecs
---
+++ utils/watt-login.py
@@ -0,0 +1,14 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+WATT_TIME_USERNAME = os.getenv('WATT_TIME_USERNAME')
+WATT_TIME_PASSWORD = os.getenv('WATT_TIME_PASSWORD')
+
+
+login_url = 'https://api.watttime.org/login'
+rsp = requests.get(login_url, auth=HTTPBasicAuth(WATT_TIME_USERNAME,WATT_TIME_PASSWORD))
+TOKEN = rsp.json()['token']
+print(rsp.json())
